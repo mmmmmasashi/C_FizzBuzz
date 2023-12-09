148bbfdff2202b1895bbea414acb2ac69f1a988f
m_masashi <drift_tms@yahoo.co.jp>
2023-12-09 16:46:23 +0900

    失敗する単体テストを1つ作った

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "CppUTest/CommandLineTestRunner.h"
 
+TEST_GROUP(FirstTestGroup){};
+
+TEST(FirstTestGroup, FirstTest)
+{
+    FAIL("Fail me!");
+}
+
 int main(int ac, char **av)
 {
     return CommandLineTestRunner::RunAllTests(ac, av);
