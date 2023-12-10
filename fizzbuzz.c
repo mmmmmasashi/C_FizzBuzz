53bc88950c64229b8d5f53cc83a298fb99d254f2
m_masashi <drift_tms@yahoo.co.jp>
2023-12-10 23:20:36 +0900

    3の倍数をFizzと出力する処理を実装

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+
 #include "fizzbuzz.h"
 
+/// @brief FizzBuzzのルールに従い、指定された数値を文字列に変換する
+/// @param number 数値
+/// @param buffer 文字列を格納するバッファ
+/// @param buffer_size バッファのサイズ(文字列の最大長)
 void ToFizzBuzzNumber(int number, char *buffer, int buffer_size)
 {
+    snprintf(buffer, buffer_size, "Fizz");
 }
