1e1fc2a002bd8f36e3058c40bea583490747d731
m_masashi <drift_tms@yahoo.co.jp>
2023-12-10 22:50:55 +0900

    成功する文字列比較のテストをお試し追加

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ TEST(FirstTestGroup, FirstTest)
     FAIL("Fail me!");
 }
 
+TEST(FirstTestGroup, SecondTest)
+{
+    STRCMP_EQUAL("hello world!", "hello world!");
+}
+
 int main(int ac, char **av)
 {
     return CommandLineTestRunner::RunAllTests(ac, av);
