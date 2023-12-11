99f0c3a9e8ed598c5214c2bf116d5185f9da160c
m_masashi <drift_tms@yahoo.co.jp>
2023-12-11 22:56:58 +0900

    main,fizzbuzzの2モジュールで構成される、TDDのテンプレート

--- main.c
+++ main.c
@@ -3,13 +3,6 @@
 
 int main(void)
 {
-    const int RepeatCount = 100;
-    for (int i = 0; i < RepeatCount; i++)
-    {
-        int number = i + 1;
-        char buffer[100]; // 十分とっておく
-        ToFizzBuzzNumber(number, buffer, sizeof(buffer));
-        printf("%s\n", buffer);
-    }
+    printf("Hello World\n");
     return 0;
 }
--- unittest.cpp
+++ unittest.cpp
@@ -1,44 +1,16 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "fizzbuzz.h"
 
-TEST_GROUP(FizzBuzzTestGroup){};
+TEST_GROUP(SampleTestGroup){};
 
-/// @brief 3の倍数の場合はFizzを返す
-TEST(FizzBuzzTestGroup, ReturnsFizzWhenMultipleOf3)
+TEST(SampleTestGroup, AlwaysFailsFunction)
 {
-    char buffer[10];
-    ToFizzBuzzNumber(3, buffer, sizeof(buffer));
-    STRCMP_EQUAL("Fizz", buffer);
+    STRCMP_EQUAL("TextA", "TextB");
 }
 
-/// @brief 5の倍数の場合はBuzzを返す
-TEST(FizzBuzzTestGroup, ReturnsFizzWhenMultipleOf5)
+TEST(SampleTestGroup, AlwaysSuccessFunction)
 {
-    char buffer[10];
-    ToFizzBuzzNumber(5, buffer, sizeof(buffer));
-    STRCMP_EQUAL("Buzz", buffer);
-}
-
-/// @brief  3と5の倍数の場合はFizzBuzzを返す
-TEST(FizzBuzzTestGroup, ReturnsFizzBuzzWhenMultipleOf3And5)
-{
-    char buffer[10];
-    ToFizzBuzzNumber(15, buffer, sizeof(buffer));
-    STRCMP_EQUAL("FizzBuzz", buffer);
-}
-
-/// @brief  3でも5でもない場合はそのままの数値を文字列に変換して返す
-TEST(FizzBuzzTestGroup, ReturnsNumberWhenNotMultipleOf3And5)
-{
-    char buffer[10];
-    ToFizzBuzzNumber(1, buffer, sizeof(buffer));
-    STRCMP_EQUAL("1", buffer);
-
-    ToFizzBuzzNumber(2, buffer, sizeof(buffer));
-    STRCMP_EQUAL("2", buffer);
-
-    ToFizzBuzzNumber(7, buffer, sizeof(buffer));
-    STRCMP_EQUAL("7", buffer);
+    CHECK_EQUAL(1, 1);
 }
 
 int main(int ac, char **av)
